881f2a985af3ece208acc02fd6ad9f5efb76c5a1
Hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2019-05-14 10:51:38 +1200

    add conditionals.py

---
+++ classes/conditionals.py
@@ -0,0 +1,19 @@
+'''
+Author=Justin
+Date=19.5.13
+'''
+
+#--Import--
+
+
+
+#--Main--
+
+name = input('''Please enter your name
+''')
+age = input('''
+Please enter your age
+''')
+
+print("\nYour name is: {} and you are {} years old".format(name, age),end='')
+
