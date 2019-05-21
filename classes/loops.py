7da8a01e0219f0a0e547f876b2911a57e3d76185
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2019-05-21 16:02:00 +1200

    add loops.py

---
+++ classes/loops.py
@@ -0,0 +1,21 @@
+'''
+Author: Justin
+Date: 19/5/20
+'''
+
+#--Librarys--
+
+
+
+#--Main--
+
+print( "Hi" )
+age = 19
+while( True ):
+    print( "Hello {}".format(age) )
+    print( "Also in the loop" )
+    age -= 1
+    if( age < 1 ):
+        break
+print( "Out of loop" )
+
